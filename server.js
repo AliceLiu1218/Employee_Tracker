20e68dcffd9db58fcfb387048a5e559d6daa5703
unknown <AliceLiu1218>
2023-08-13 09:18:00 -0400

    setup inqurier questions

--- server.js
+++ server.js
@@ -23,6 +23,71 @@ const db = mysql.createConnection(
 );
 
 
+//Add Inquirer prompts questions 
+const inquirer = require('inquirer');
+inquirer
+		.prompt([
+		{
+			// List type to allow user for choosing between options.
+			type: 'list',
+			name: 'action',
+			message: 'What would you like to do?',
+			choices: [
+			
+			'View ALL Employees.',
+            'Add Employee.',
+            'Update Employee Role.',
+            'View ALL Roles.',
+            'Add Role.',
+            'View ALL Departments.',
+			'Add Department.',
+			
+			],
+		},
+		])
+    	// Then function to pass user inputs as answer param.
+		.then((answer) => {
+		// Call respective functions based on user's choice through param.
+		switch (answer.action) {
+            case 'View ALL Employees.':
+			ViewAllEmployees();
+            break;
+
+            case 'Add Employee.':
+			AddEmployee();
+			break;
+
+            case 'Update Employee Role.':
+			UpdateEmployeeRole();
+			break;
+
+            case 'View ALL Roles.':
+			ViewAllRoles();
+			break;
+
+            case 'Add Role.':
+			AddRole();
+			break;
+
+			case 'View ALL Departments.':
+			ViewAllDepartments();
+			break;
+			
+			case 'Add Department.':
+			AddDepartment();
+			break;
+			
+			default:
+			console.log('Invalid choice! Please select again...');
+			break;
+		}
+		})
+		// Catch function for any potential errors.
+		.catch((error) => {
+			console.log('An error occurred:', error);
+			connection.end();
+		});
+
 
 
 // Start the server.
